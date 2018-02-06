d05671d6fcd9511559781bd2a8d55c369323323d
Tim <bkkkd@163.com>
2018-02-07 00:25:26 +0800

    完成脚本

---
+++ build-phar.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Short description for build-phar.php
+ *
+ * @package build-phar
+ * @author tim <tim@tim-notebook>
+ * @version 0.1
+ * @copyright (C) 2018 tim <tim@tim-notebook>
+ * @license MIT
+ */
+
+// 参数内容为生成文件路径 此例中则在当前目录生成example.phar打包程序
+$filename = __DIR__.'/copyrightdoc.phar';
+if(file_exists($filename)){
+    unlink($filename);
+}
+$phar = new Phar($filename);    
+// 开始打包
+$phar->startBuffering();  
+// 要打包的目的目录 绝对路径
+$phar->buildFromDirectory(__DIR__);  
+// 压缩方式 GZ->gzip  BZ2->bz2
+$phar->compressFiles(Phar::GZ);    
+// 设置启动加载脚本 即导入phar之后第一个自动执行的脚本
+$phar->setStub('#!/usr/bin/env php'.PHP_EOL.$phar->createDefaultStub('copyrightdoc.php'));  
+// 结束打包
+$phar->stopBuffering();
---
+++ copyrightdoc.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Short description for copyrightdoc.php
+ *
+ * @package copyrightdoc
+ * @author tim <tim@tim-notebook>
+ * @version 0.1
+ * @copyright (C) 2018 tim <tim@tim-notebook>
+ * @license MIT
+ */
+$short_opt = 'd:';
+$short_opt .= 'o:';
+$short_opt .= 'e:';
+$short_opt .= 'x:';
+$short_opt .= 'h';
+
+$options = getopt($short_opt);
+
+$help = false;
+if(isset($options['h'])){
+    $help = true;
+}
+if($help){
+    help();
+}
+
+$dir = $_SERVER['PWD'];
+if(!empty($options['d'])){
+    $dir = realpath($options['d']);
+}
+
+put('dir:'.$dir);
+
+$exts = ['php','tpl'];
+if(!empty($options['e'])){
+    if(is_array($options['e'])){
+
+        $exts = $options['e'];
+    }elseif(is_string($options['e'])){
+        $exts = [$options['e']];
+    }
+
+
+}
+put('ext:'.implode(',', $exts));
+
+$excludes = ['.git','tmp'];
+if(!empty($options['x'])){
+    if(is_array($options['x'])){
+        $excludes = $options['x'];
+    }elseif(is_string($options['x'])){
+        $excludes = [$options['x']];
+    }
+}
+put('excludes:'.implode(',', $excludes));
+
+$outfile = basename($dir).'.docx';
+if(!empty($options['o'])){
+    $outfile = $options['o'];
+}
+put('outfile:'. $outfile);
+
+function put($msg=""){
+    fwrite(STDOUT, $msg."\n");
+}
+function help(){
+    put('Help message');
+    put('------------');
+    put('omniconfig -d /your/path/to [-h]');
+    put('  -d set a new config name.default is DB_DATABASE');
+    put('  -h this message');
+    exit;
+
+}
+function get_allfiles($path,&$files) {  
+    if(is_dir($path)){  
+        $dp = dir($path);  
+        while ($file = $dp ->read()){  
+            if($file !="." && $file !=".."){  
+                get_allfiles($path."/".$file, $files);  
+            }  
+        }  
+        $dp ->close();  
+    }  
+    if(is_file($path)){  
+        $files[] =  $path;  
+    }  
+}  
+$files = [];
+get_allfiles($dir, $files);
+
+include_once __DIR__.'/vendor/autoload.php';
+
+$phpWord = new \PhpOffice\PhpWord\PhpWord();
+$section = $phpWord->addSection();
+$dir_lenght = strlen($dir);
+
+foreach($files as $file){
+    $jump = false;
+    foreach($excludes as $ex){
+        if(strpos($file, $ex)!==false){
+            $jump = true;
+            break;
+        }
+    }
+    $ext = substr(strrchr(strtolower($file), '.'), 1);
+    if(!in_array($ext, $exts)){
+        $jump = true;
+    }
+
+    if($jump){
+        continue;
+    }
+    $filepath = substr($file, $dir_lenght+1);
+    put($filepath);
+    $filedb = file($file);
+    $section->addText('file path:'.trim(htmlspecialchars($filepath)));
+    foreach($filedb as $line){
+        if(!trim($line)){
+            continue;
+        }
+        $section->addText(trim(htmlspecialchars($line)));
+    }
+}
+$objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+$objWriter->save($outfile);
